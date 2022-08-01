642613cf88cdac076abf3378269089408f191db4
zainal salamun <zainalrtf@gmail.com>
2022-08-01 15:22:53 +0700

    Create class api, setup structure Folder, setup material Getx, setup color theme model, setup session

---
+++ lib/config/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const baseUrl = '';
+  static const history = '$baseUrl/history';
+  static const user = '$baseUrl/user';
+}
---
+++ lib/config/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primary = Color(0xff767ae7);
+  static const secondary = Color(0xffb9fffc);
+  static const bg = Color.fromARGB(255, 187, 204, 252);
+  static const chart = Color(0xffa3d8f4);
+}
